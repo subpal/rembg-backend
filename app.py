872812e5992e1f3d8d4f3872d2baf8fcc89d4bcd
subpal <subhampal198@gmail.com>
2024-10-09 11:29:43 +0530

    New api

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 from rembg import remove
 from flask_cors import CORS
 import io
+from transformers import pipeline
 
 app = Flask(__name__)
 CORS(app)
@@ -30,6 +31,32 @@ def remove_background():
         200,
         {'Content-Type': 'image/png'}
     )
+
+@app.route('/remove-backgroundv2', methods=['POST'])
+def remove_background_v2():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
+    
+    # Read the image file
+    image_file = request.files['image']
+    image = Image.open(image_file.stream)
+    out_path = "/Users/aashish.singh/Self/rembg/rembg-backend/outfile/"
+    pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+    pillow_image = pipe(image)
+
+
+    img_byte_arr = io.BytesIO()
+    pillow_image.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    print("Successfull api call")
+    
+    # Return the image without background as a response
+    return (
+        img_byte_arr.read(),
+        200,
+        {'Content-Type': 'image/png'}
+    )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
